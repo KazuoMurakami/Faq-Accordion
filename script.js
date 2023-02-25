2fc9004f1f91101b980f66a88a59a18c24029916
kazuo <otaviokazuo12@hotmail.com>
2023-02-25 20:10:25 -0300

    Alteração de responsividade

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const answers = document.querySelectorAll(".answer");
-answers.forEach((answer) => {
-  answer.style.display = "none"; // ocultar o texto inicialmente
-  const question = answer.previousElementSibling;
-  const arrow = question.querySelector(".rotate");
+const answer = document.querySelectorAll(".answer");
+
+answer.forEach((answer) => {
+  answer.style.display = "none";
+  const question = answer.previousElementSibling; // aqui está pegando o elemento irmao do <P> que seria o <h2>
+  const arrow = question.querySelector(".rotate"); // estou selecionando a classe rotate que está no h2 que seria a question
 
   question.addEventListener("click", () => {
     if (answer.style.display === "none") {
-      answer.style.display = "block"; // mostrar a resposta quando clicar no título
-      arrow.style.transform = "rotate(180deg)"; // girar a imagem de seta
-      answer.previousElementSibling.style.color = "black"; // muda a cor do texto do título para preto quando clicado
+      answer.style.display = "block";
+      arrow.style.transform = "rotate(180deg)"; // poderia utilizar um addclass também para esse evento
+      question.style.color = "black"; // adiciona cor preta quando eu clicar no h2
     } else {
-      answer.previousElementSibling.style.color = "hsl(240deg 3% 72%)"; // muda a cor do texto do título para preto quando clicado
-      answer.style.display = "none"; // ocultar a resposta quando clicar novamente no título
-      arrow.style.transform = "rotate(0deg)"; // girar a imagem de seta de volta à posição original
+      question.style.color = "hsl(240deg 3% 72%)";
+      answer.style.display = "none";
+      arrow.style.transform = "rotate(0deg)";
     }
   });
 });
